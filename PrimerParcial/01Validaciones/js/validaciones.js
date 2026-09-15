4624f9084cf75417624c8f628344633cd76a89cf
HernandezMalagonGael <ghernandezm2404@alumno.ipn.mx>
2026-09-14 19:00:26 -0600

    Expresiones regulares

---
+++ PrimerParcial/01Validaciones/js/validaciones.js
@@ -0,0 +1,4 @@
+/* Las validaciones para este formulario se realizaran mediante el uso de expreciones regulares, las cuales las vamos a dividir en 3, la primera en tipo texto para nombre, la segunda tipo numerico para la boleta y tercero debe tener un patron para la fecha  
+
+Las expresiones regulares son patrones que nos ayudan a validar cadenas bajo ciertas condiciones.
+*/
